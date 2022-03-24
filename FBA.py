bbedcbd89d05cd0331018e25d5f5ee2b57cb14be
BA Pessi <pessibruno@gmail.com>
2022-03-24 15:15:07 +0100

    fba

---
+++ FBA.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import linprog
+
+def get_matrix(filename):
+
+    df = pd.read_excel(filename,index_col=0)
+    rev = df.loc['reversible',:].values
+    df = df.drop('reversible')
+    feed_list = feed_excel() 
+    Aeq = df.to_numpy(na_value=0)
+    beq = np.zeros(Aeq.shape[0]) #  
+    c = np.zeros(Aeq.shape[1])
+    return df,Aeq,beq,c,rev,feed_list
+
+def get_bounds(rev):
+    bounds = []
+    b_max = 1e6 #y1
+    for r in rev:
+        if r == 1:
+            bounds.append((-b_max,b_max))
+        elif r==0:
+            bounds.append((0,b_max))
+        else:
+            print("error with reversible array")
+    return bounds
+
+def correct_feed(feed_list,bounds,c,r_max):
+    for reaction,i in zip(df.columns,range(len(df.columns))):
+        if reaction in feed_list:
+            print(reaction)
+            bounds[i] = (-1e5,1e5)
+        if reaction == r_max:
+            print('max reaction: ',reaction)
+            c[i] = -1
+    return bounds,c
+
+def feed_excel():
+    filename = 'data/feed_list.xlsx'
+    feed_list = pd.read_excel(filename).to_numpy()
+    feed_list=feed_list.flatten()
+    return feed_list
+
+if __name__ == '__main__':
+    filename = 'ecoli_core_model' 
+    r_max = 'Biomass_Ecoli_core'
+    fileinput = 'data/' + filename + '.xlsx' 
+    fileoutput = 'data/' + 'FBA_' + filename + '.xlsx'
+    df,Aeq,beq,c,rev,feed_list = get_matrix(fileinput)
+    bounds = get_bounds(rev)
+    bounds,c = correct_feed(feed_list,bounds,c,r_max)
+    res = linprog(c,A_eq=Aeq,b_eq=beq,bounds=bounds,method="interior-point",options={'tol':1e-7,'maxiter':10000})
+    print("max flux ", max(res.x))
+    print("min flux", min(res.x))
+    flux =pd.Series(res.x,index=df.columns)
+    print("Flux biomass: ",flux[r_max])
+    print("Flux glucose: ",flux['EX_glc(e)'])
+    flux.to_excel(fileoutput)
+
--- test_subnetwork.py
+++ test_subnetwork.py
@@ -2,9 +2,6 @@ import numpy as np
 import pandas as pd
 import dependency
 # This is a modification
-m_A = pd.Series(['glc-D[e]','lac-D[e]'])
-m_ignore = pd.Series(["coa",'adp','atp','h2o','h2o[e]',"h",'h[e]','nad','nadh','nadp','nadph','co2'])
-
 def get_reactions(m,df):
     reactions = df.loc[m].loc[df.loc[m]!=0]
     return pd.Series(reactions.index.values)
@@ -14,14 +11,9 @@ def get_metabolites(r,df):
     return pd.Series(metabolites)
 
 def remove_checked(new,checked,actual,ignore=[],m_A=[]):
-    print('antes ', new)
     remove = list(set(new)&set(checked))
-    for r in remove:
-        new.remove(r)
-    for n in new:
-        if n in ignore or n in m_A or n in actual:
-            new.remove(n)
-    print('depois ', new)
+    new = new.loc[~new.isin(checked)].loc[~new.isin(actual)]
+    new = new.loc[~new.isin(ignore)].loc[~new.isin(m_A)]
     return new
 
 def initiate_chain(m_init,df):
@@ -29,25 +21,30 @@ def initiate_chain(m_init,df):
     return r_init
 
 def get_reaction_chain(r_unchecked,df):
-    m_unchecked = []
-    m_checked = []
-    r_checked = []
-    while r_unchecked:
+
+    m_unchecked = pd.Series(dtype='object') 
+    m_checked = pd.Series(dtype='object') 
+    r_checked = pd.Series(dtype='object') 
+
+    while not r_unchecked.empty:
         for r in r_unchecked:
             m_new = get_metabolites(r,df)
-            m_unchecked = m_unchecked + remove_checked(m_new.copy(),m_checked.copy(),m_unchecked.copy(),m_ignore.copy(),m_A.copy())
-            r_checked.append(r)
+            m_unchecked = pd.concat([m_unchecked,remove_checked(m_new,m_checked,m_unchecked,m_ignore,m_A)],ignore_index=True) 
+            r_checked = pd.concat([r_checked,pd.Series([r])],ignore_index=True)
+        r_unchecked = pd.Series(dtype='object') 
 
-        r_unchecked = []
         for m in m_unchecked:
             r_new = get_reactions(m,df) 
-            r_unchecked = r_unchecked + remove_checked(r_new.copy(),r_checked.copy(),r_unchecked.copy())
-            m_checked.append(m)
-        m_unchecked = []
+            r_unchecked = pd.concat([r_unchecked,remove_checked(r_new,r_checked,r_unchecked)],ignore_index=True)
+            m_checked = pd.concat([m_checked,pd.Series([m])],ignore_index=True)
+        m_unchecked = pd.Series(dtype='object') 
 
     return r_checked,m_checked
 
 if __name__ == '__main__':
+    m_A = pd.Series(['pyr','glc-D[e]','lac-D[e]','gln-L','glu-L','oaa'])
+    m_ignore = pd.Series(["coa",'adp','atp','h2o','h2o[e]',"h",'h[e]','nad','nadh','nadp','nadph','co2'])
+
 
     print('START')
     file_input = 'ecoli_core_model.xlsx'
@@ -55,9 +52,9 @@ if __name__ == '__main__':
 
     m_init = 'pyr'
     r_init = initiate_chain(m_init,df)
-    print('n of r init: ', len(r_init))
     r_chain,m_checked = get_reaction_chain(r_init,df)
     print(r_chain)
     print("n of reactions: ",len(r_chain))
     print(len(set(r_chain)))
-
+    df_reactions = pd.Series(df.columns.values)  
+    print(df_reactions.loc[~df_reactions.isin(r_chain)])
